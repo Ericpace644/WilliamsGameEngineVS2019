6672333bc8401a613f30038189b6b5cdf6b24a9b
Ericpace644 <90404067+Ericpace644@users.noreply.github.com>
2022-04-28 08:54:48 -0700

    nrdhny5e

---
+++ Base project/MyGame/MyGame/Ship.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include  "Engine/GameEngine.h"
+
+class Ship : public GameObject
+{
+public:
+	//Creats our chip
+	Ship();
+
+	//Functions overridden from GameObject;
+	void draw();
+	void update(sf::Time& elapsed);
+private:
+	sf::Sprite sprite_;
+};
+
+typedef std::shared_ptr<Ship> ShipPtr;

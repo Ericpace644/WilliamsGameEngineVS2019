9723e6929cf46dc7863fe6572b5ea02a77c841a2
Ericpace644 <90404067+Ericpace644@users.noreply.github.com>
2022-04-29 09:06:13 -0700

    gfrsbhrt

--- Base project/MyGame/MyGame/Ship.cpp
+++ Base project/MyGame/MyGame/Ship.cpp
@@ -1,6 +1,7 @@
 #include "Ship.h"
 
 const float SPEED = 0.3f;
+const int FIRE_DELAY = 200;
 
 void Ship::update(sf::Time& elapse) {
 	sf::Vector2f pos = sprite_.getPosition();
@@ -15,6 +16,22 @@ void Ship::update(sf::Time& elapse) {
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) x += SPEED * msElapsed;
 
 	sprite_.setPosition(sf::Vector2f(x, y));
+
+	if (fireTimer_ > 0)
+	{
+		fireTimer_ -= msElapsed;
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) && fireTimer_ <= 0)
+	{
+		fireTimer_ = FIRE_DELAY;
+		sf::FloatRect bounds = sprite_.getGlobalBounds();
+
+		float laserX = x + bounds.width;
+		float laserY = y + (bounds.height / 2.0f);
+
+		LaserPtr laser = std::make_shared<laser>(sf::Vector2f(laserX, laserY));
+	}
 }
 Ship::Ship()
 {
